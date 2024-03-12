e7894ca6ebcf24d3799ad01e8ca714f38bba9e08
shaon1087 <shaonsaha1087@gmail.com>
2024-03-12 12:43:13 +0600

    index and javascript file added

---
+++ js/hunter.js
@@ -0,0 +1,10 @@
+const loadPhone =  async async => {
+    const res = await fetch(
+      'https://openapi.programming-hero.com/api/phones?search=iphone'
+    );
+    const data = await res.json();
+    console.log(data.data); 
+
+
+}
+loadPhone();
