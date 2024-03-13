63a9cc554e7e0d4536ccfc06590550df342d366a
shaon1087 <shaonsaha1087@gmail.com>
2024-03-13 20:37:44 +0600

    added js

--- js/hunter.js
+++ js/hunter.js
@@ -1,10 +1,52 @@
-const loadPhone =  async async => {
-    const res = await fetch(
-      'https://openapi.programming-hero.com/api/phones?search=iphone'
-    );
-    const data = await res.json();
-    console.log(data.data); 
+const loadPhone = async (searchText) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  );
+  const data = await res.json();
+  const phones = data.data;
+  displayPhone(phones);
+};
+const displayPhone = (phones) => {
+  const phoneContainer = document.getElementById("phone-container");
+  // clear the container before adding new cards
+  phoneContainer.textContent = '';
+  const showButton = document.getElementById('show-container');
+   if(phones.length > 10) {
+      showButton.classList.remove('hidden');
+   }
+   else{
+    showButton.classList.add('hidden');
+   }
+  // display only 10 phones
 
+  phones = phones.slice(0, 10);
+ 
+  phones.forEach((phone) => {
+    console.log(phone);
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card bg-gray-100  p-4 shadow-xl`;
+    phoneCard.innerHTML = `
+        <figure>
+              <img
+                src=${phone.image}
+                alt="Shoes"
+              />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">${phone.phone_name}</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+        `;
+    phoneContainer.appendChild(phoneCard);
+  });
+};
 
-}
-loadPhone();
+// handle search button
+const handleSearch = () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  loadPhone(searchText);
+};
